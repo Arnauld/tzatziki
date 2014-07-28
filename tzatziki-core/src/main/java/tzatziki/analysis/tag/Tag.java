e7d6e017026271f28ac89e15bba6bfaae7d7db7a
Arnauld <arnauld.loyer@gmail.com>
2014-07-28 12:33:06 +0200

    remove uncessary for now

--- tzatziki-core/src/main/java/tzatziki/analysis/tag/Tag.java
+++ tzatziki-core/src/main/java/tzatziki/analysis/tag/Tag.java
@@ -1,18 +1,12 @@
 package tzatziki.analysis.tag;
 
 
-import com.google.common.collect.FluentIterable;
-import com.google.common.collect.Sets;
-
-import java.util.Set;
-
 /**
  * @author <a href="http://twitter.com/aloyer">@aloyer</a>
  */
 public class Tag {
     private final String tag;
     private String description;
-    private final Set<UsedBy> usedBySet = Sets.newHashSet();
 
     public Tag(String tag) {
         this.tag = tag;
@@ -22,14 +16,6 @@ public class Tag {
         return tag;
     }
 
-    public Tag declareUsedBy(UsedBy usedBy) {
-        usedBySet.add(usedBy);
-        return this;
-    }
-
-    public FluentIterable<UsedBy> usages() {
-        return FluentIterable.from(usedBySet);
-    }
 
     public Tag declareDescription(String description) {
         this.description = description;
